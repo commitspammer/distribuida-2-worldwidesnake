20b20c9616d26561359629039bb24d9a9fe9f63b
Alexandre Cesar <guimaraes.aco@gmail.com>
2023-05-20 16:31:59 -0300

    update game functions added

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,54 +1,136 @@
 import kaboom from "kaboom";
 
 kaboom({
-  background: [0, 0, 0, 1],
+  background: [28, 26, 28, 1],
 });
 
 const API_URL = "http://localhost:8080";
-
-const gameEvents = new EventSource("http://localhost:8080/game/events");
-
-gameEvents.addEventListener("newState", (e) => {
-  const state = JSON.parse(e.data);
-  updateGameScene(state);
-});
-
 const block_size = 20;
 
-function createBoard(height, width) {
-  const board = [];
+scene("main", () => {
+  const gameEvents = new EventSource(`${API_URL}/game/events`);
 
-  // Create top wall
-  const topWall = "=".repeat(width + 2);
-  board.push(topWall);
+  gameEvents.addEventListener("newState", (e) => {
+    const state = JSON.parse(e.data);
+    updateGameScene(state);
+  });
 
-  // Create middle rows with side walls
-  for (let row = 1; row < height + 1; row++) {
-    let rowString = "=";
-    for (let col = 1; col < width + 1; col++) {
-      rowString += " ";
+  function createBoard(height, width) {
+    const board = [];
+
+    const topWall = "=".repeat(width + 2);
+    board.push(topWall);
+
+    for (let row = 1; row < height + 1; row++) {
+      let rowString = "=";
+      for (let col = 1; col < width + 1; col++) {
+        rowString += " ";
+      }
+      rowString += "=";
+      board.push(rowString);
     }
-    rowString += "=";
-    board.push(rowString);
+
+    const bottomWall = "=".repeat(width + 2);
+    board.push(bottomWall);
+
+    return board;
   }
 
-  // Create bottom wall
-  const bottomWall = "=".repeat(width + 2);
-  board.push(bottomWall);
+  function updateGameScene(state) {
+    const { rows, cols, snakes, foods } = state;
 
-  return board;
-}
+    updateBoardSize(rows, cols);
+    updateSnakes(snakes);
+    updateFoods(foods);
+  }
 
-scene("main", () => {
-  const map = addLevel(createBoard(40, 40), {
-    width: block_size,
-    height: block_size,
-    pos: vec2(0, 0),
-    "=": () => [rect(block_size, block_size), color(255, 0, 0), area(), "wall"],
-  });
+  function updateBoardSize(rows, cols) {
+    const map = addLevel(createBoard(rows, cols), {
+      width: block_size,
+      height: block_size,
+      pos: vec2(0, 0),
+      "=": () => [
+        rect(block_size, block_size),
+        color(255, 0, 0),
+        area(),
+        "wall",
+      ],
+    });
+  }
+
+  function updateSnakes(snakes) {
+    destroy("snake");
+
+    snakes.forEach((snake) => {
+      const { name, head, tail, facing } = snake;
+
+      const snakeHead = add([
+        rect(block_size, block_size),
+        pos(head.x * block_size, head.y * block_size),
+        color(125, 124, 125),
+        area(),
+        { name: `snake-${name}-head`, type: "snake" },
+      ]);
+
+      tail.forEach((tailSegment) => {
+        const snakeTail = add([
+          rect(block_size, block_size),
+          pos(tailSegment.x * block_size, tailSegment.y * block_size),
+          color(255, 255, 255),
+          area(),
+          { type: "snake" },
+        ]);
+      });
+    });
+  }
+
+  function updateFoods(foods) {
+    destroy("food");
+
+    foods.forEach((food, index) => {
+      const foodEntity = add([
+        rect(block_size, block_size),
+        pos(food.x * block_size, food.y * block_size),
+        color(0, 255, 0),
+        area(),
+        "food",
+      ]);
+    });
+  }
+
+  const mockState = {
+    rows: 35,
+    cols: 35,
+    snakes: [
+      {
+        name: "snake1",
+        head: { x: 5, y: 5 },
+        tail: [
+          { x: 4, y: 5 },
+          { x: 3, y: 5 },
+        ],
+        facing: "right",
+      },
+      {
+        name: "snake2",
+        head: { x: 10, y: 15 },
+        tail: [
+          { x: 10, y: 16 },
+          { x: 10, y: 17 },
+        ],
+        facing: "up",
+      },
+    ],
+    foods: [
+      { x: 8, y: 8 },
+      { x: 12, y: 20 },
+    ],
+  };
+
+  updateGameScene(mockState);
 });
 
-scene("teste", () => {
+scene("login", () => {
   let curSize = 48;
   const pad = 24;
 
@@ -59,6 +141,12 @@ scene("teste", () => {
       align: "center",
       lineSpacing: 8,
       letterSpacing: 1,
+      transform: (idx, ch) => ({
+        color: hsl2rgb((time() * 0.2 + idx * 0.1) % 1, 0.7, 0.8),
+        pos: vec2(0, wave(-4, 4, time() * 4 + idx * 0.5)),
+        scale: wave(1, 1.2, time() * 3 + idx),
+        angle: wave(-9, 9, time() * 3 + idx),
+      }),
     }),
     pos(width() / 2, height() / 2 - 50),
     origin("center"),
@@ -75,28 +163,22 @@ scene("teste", () => {
       letterSpacing: 1,
     }),
   ]);
-
-  // Like onKeyPressRepeat() but more suitable for text input.
   onCharInput((ch) => {
     input.text += ch;
   });
 
-  // Like onKeyPress() but will retrigger when key is being held (which is similar to text input behavior)
-  // Insert new line when user presses enter
   onKeyPressRepeat("enter", () => {
     postData(input.text)
       .then((response) => {
         console.log("API response:", response);
         go("main");
-        // Handle the API response here
       })
       .catch((error) => {
         console.error("Error:", error);
-        // Handle the error here
+        go("main");
       });
   });
 
-  // Delete last character
   onKeyPressRepeat("backspace", () => {
     input.text = input.text.substring(0, input.text.length - 1);
   });
@@ -120,4 +202,4 @@ scene("teste", () => {
   }
 });
 
-go("teste");
+go("login");
